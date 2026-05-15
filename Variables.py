080404376548a02cd0e211d8f06b25997eb6985d
esportacusx34 <juanestebanardilamartinez84@gmail.com>
2026-05-15 13:10:17 -0500

    Add variables and print their types and values
    Fix print statements and variable type comments.

---
+++ Variables.py
@@ -0,0 +1,21 @@
+#Esto es un comentario 
+"""esto es un comentario 
+de varias lineas"""
+nombre="juan esteban ardila Martínez"
+edad=14
+estado=true 
+nota=42
+
+#como saber el tipo de dato de cada variable 
+
+Print (type(nombre)) #resultado:<class:"str">
+
+Print (type(edad)) #resultado <class:'int'>
+Print (type(estado))#resultado<class:'bool'
+Print (type(nota))#resuktado<class:'float'
+
+#mostrar el valor que contiene cada variable 
+Print(nombre)
+Print(edad)
+Print(estado)
+Print(nota)
